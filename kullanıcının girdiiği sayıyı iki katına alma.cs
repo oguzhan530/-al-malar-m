3510a6316cbebc92e0cc69757e3131fe2589caa2
oguzhan530 <oguzhanuluugur1905@gmail.com>
2021-04-18 15:30:29 +0300

    Create kullanıcının girdiiği sayıyı iki katına alma.cs

---
+++ kullanıcının girdiiği sayıyı iki katına alma.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace kullanicinin_girdiği_sayının__iki_katını_alma
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int sayi;
+            Console.Write("Bir sayı giriniz:");
+            sayi = Convert.ToInt32(Console.ReadLine());
+            sayi = sayi * 2;
+            Console.WriteLine("Sonuc:"+sayi);
+        }
+    }
+}
