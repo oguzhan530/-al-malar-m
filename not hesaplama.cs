f836f9b0db3a04af18da820612ebf13cdf40852e
oguzhan530 <oguzhanuluugur1905@gmail.com>
2021-04-18 14:03:14 +0300

    not hesaplama

---
+++ not hesaplama.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+
+        {
+            
+            Console.WriteLine("Ara sınav Notunuzu girniz");
+            int arasnv = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Finanl notunuzu giriniz");
+            int finalsnv = Convert.ToInt32(Console.ReadLine());
+            double ort = Convert.ToInt32(arasnv * 0.4 + finalsnv * 0.6);
+            Console.WriteLine("Ortalamanız={0}", ort);
+            if (finalsnv <= 45)
+            {
+                Console.Write("ff");
+            }
+            if (ort>=0 && ort<=29)
+            {
+               
+                Console.Write("Harf notunuz=", harf);
+                
+            }
+            else if (ort>=30&&ort<=39)
+            {
+                
+                Console.Write("Harf notunuz=", harf);
+            }
+        }
+    }
+}
